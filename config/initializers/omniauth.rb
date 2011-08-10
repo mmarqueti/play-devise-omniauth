c0b88a6582f1b1d850a0f1114f1305b6ee670387
mmarqueti <marcelomarqueti@gmail.com>
2011-08-10 12:04:51 -0300

    first commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'qmHpWvhcCXDCyxI1NrfmA', '5943rg7o5nR1og9qAiItLvZhT2E5VNutQpWnlbXcQ40'
-  provider :facebook, '142565224137', '7f008819fa41d5dc6ee7dcf048370049'
+  provider :twitter, 'KEY', 'SECRET'
+  provider :facebook, 'KEY', 'SECRET'
 end
